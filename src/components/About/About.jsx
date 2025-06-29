056414ae7c75007d0d804eb82675283d293516c8
Tripti <tripsy1625@gmail.com>
2025-06-29 14:55:51 +0530

    Image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/Tripti_Image_Linkedin";
+import profileImage from "../../assets/Tripti_Image_Linkedin.jpg";
 
 const About = () => {
   return (
